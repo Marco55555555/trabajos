684500ad582ccdc414aca5cfd6c6c3aa73527acc
marco <marco2002natacion@gmail.com>
2023-09-28 09:52:12 -0500

    first commit

---
+++ t1.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def norma (v,p):
+    a = v[p]
+    num = np.linalg.norm(a)
+    return num **2
+
+def sum_prov (v,p):
+    suma = 0
+    for p in range (len(v)):
+        sum1 = suma + norma (v,p)
+        suma = sum1
+    return suma
+
+def prov (v,p):
+    num = norma (v,p)
+    den = sum_prov (v,p)
+    return num/den
+
+def transicion (v1,v2):
+    v1n = v1 / norma **(1/2)    
+    v2n = v2 / norma **(1/2)
+    r = np.transpose(np.conjugate(v2n))
+    o = np.dot(r*v1n)
+    return o
+
